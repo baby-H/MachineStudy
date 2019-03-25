b19db0715ea86feb36a83fe522c020ae5b35fb37
baby <baby@babydeMacBook-Pro.local>
2019-03-25 22:19:48 +0800

    本地文字转语音

--- sounds/sound_record.py
+++ sounds/sound_record.py
@@ -9,11 +9,11 @@ from sounds import xunfei_ontime_sound_text
 # Recognizer().listen(Microphone()) 指定使用 Microphone() 参数 device_index=default
 if __name__ == '__main__':
     print(sr.Microphone.list_microphone_names())
-    client = xunfei_ontime_sound_text.Client()
+    # client = xunfei_ontime_sound_text.Client()
     mic = sr.Microphone()
     r = sr.Recognizer()
     with mic as source:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
-        print('录音结束，开始recognize...')
-        client.send(audio)
+        # print('录音结束，开始recognize...')
+        # client.send(audio)
