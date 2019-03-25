dbc539c264306a7bc58f63c75d3b6c26f71a71d9
huyingjie <huyj@tongc-soft.com>
2019-03-25 17:16:10 +0800

    代码错误修改和新增resource

--- sounds/sound_record.py
+++ sounds/sound_record.py
@@ -2,9 +2,8 @@
 # Author: Hu Ying Jie ( huyingjie2123@163.com )
 
 import speech_recognition as sr
-import sys
-sys.path.append('E:/')
 from sounds import xunfei_ontime_sound_text
+
 
 # Recognizer() 使用系统默认
 # Recognizer().listen(Microphone()) 指定使用 Microphone() 参数 device_index=default
@@ -16,5 +15,4 @@ if __name__ == '__main__':
     with mic as source:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
-        temp = client(audio)
-        print(temp)
+        client.send(audio)
